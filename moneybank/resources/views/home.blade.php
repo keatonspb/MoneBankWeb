d117d02073fa4532b92657824ad1db65d8e861f6
Бычковский Константин <bk@101media.ru>
2017-03-01 00:09:07 +0300

    Initial commit

---
+++ moneybank/resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+
+    <div class="row">
+        <div class="col-lg-3">
+            <div class="panel panel-default">
+                <div class="panel-heading">Сводка</div>
+                <div class="panel-body">
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-9"></div>
+    </div>
+</div>
+@endsection
